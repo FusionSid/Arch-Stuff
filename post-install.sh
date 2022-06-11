df4798fda0e2a61f3a7b3ceb12478b7999bf5ec1
FusionSid <siddheshadsv@icloud.com>
2022-06-11 14:25:50 +1200

    Fix?

--- post-install.sh
+++ post-install.sh
@@ -1,13 +1,14 @@
+read -p "Enter disk name: " disk_name
 pacman -Syu
 ln -sf /usr/share/zoneinfo/Pacific/Auckland /etc/localtime
 sed -i.backup -e "170 s/^#//" /etc/locale.gen
-echo "LANG=en.NZ_UTF-8" >> /etc/locale.conf
-pacman -S ntp zsh wget tmux wget vim neofetch lolcat
+echo "LANG=en.NZ_UTF-8" > /etc/locale.conf
+pacman -S ntp zsh wget tmux wget vim neofetch lolcat git --noconfirm
 ntpd -qg
 hwclock --systohc
 locale-gen
-echo "KEYMAP=us" >> /etc/vconsole.conf
-echo "fusionsid" >> /etc/hostname
+echo "KEYMAP=us" > /etc/vconsole.conf
+echo "fusionsid" > /etc/hostname
 
 clear
 echo "Set root password:"
@@ -30,26 +31,25 @@ grub-mkconfig -o /boot/grub/grub.cfg
 # cp -r dotfiles/* ~/.config
 # cp .zshrc ~/.zshrc
 
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd
-rm -rf yay
+# i cant run this in sudo :(
+# git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd
+# rm -rf yay
 
 while true; do
     read -p "gnome or kde?" desktop # idk how to use any other desktops lol
     if [ $desktop == 'gnome' ]; then 
-        pacman -S xorg gnome gnome-tweaks;
+        pacman -S xorg gnome gnome-tweaks --noconfirm;
         systemctl enable gdm; 
         break;
     if [ $desktop == 'kde' ]; then 
-        pacman -S xorg plasma konsole packagekit-qt5 sddm; 
+        pacman -S xorg plasma konsole packagekit-qt5 sddm --noconfirm; 
         systemctl enable sddm; 
         break;
     if [ $desktop == 'none' ]; then 
         break;
     else
-        echo 'Not a valid choice'
+        echo 'Not a valid choice skill issue'
     fi
 done
 
 exit
-umount -a
-reboot
