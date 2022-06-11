7d680a1e44fc1a29730ec0801b32897a8242a047
FusionSid <siddheshadsv@icloud.com>
2022-06-12 01:46:16 +1200

    Fixed zshrc and ohmyzsh

---
+++ .zshrc
@@ -0,0 +1,15 @@
+# Oh My Zsh stuff
+export ZSH="usr/share/.oh-my-zsh"
+
+ZSH_THEME="robbyrussell"
+
+plugins=(
+  git
+)
+
+ZSH_CACHE_DIR=$HOME/.config/oh-my-zsh
+if [[ ! -d $ZSH_CACHE_DIR ]]: then
+    mkdir $ZSH_CACHE_DIR
+fi
+
+source $ZSH/oh-my-zsh.sh
--- post-install.sh
+++ post-install.sh
@@ -2,19 +2,19 @@
 sudo mkdir -p ~/.config
 sudo cp -r /Arch-Stuff/dotfiles/* ~/.config
 
-# Oh my zsh
-bash $(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)
-
 # Install Yay
 cd ~/ && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd && rm -rf yay
+
+# Oh my zsh
+yay -S oh-my-zsh-git --noconfirm
 
 # Kitty font
 sudo wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip" -O /Arch-Stuff/fc.zip
 sudo mkdir /usr/share/fonts/FiraCode
 sudo unzip /Arch-Stuff/fc.zip -d /usr/share/fonts/FiraCode
 
-# Delete folder
-sudo rm -rf /Arch-Stuff
+# zshrc
+cat /Arch-Stuff/.zshrc >> ~/.zshrc
 
 # End
-echo "Setup complete!\nType `exit` then `reboot` to complete""
+echo "Setup complete!\nType `exit` then `reboot` to complete"
